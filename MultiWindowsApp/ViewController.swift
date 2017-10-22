cf31fc078b83b6f8fad69511e8715dd0070f3f92
Alexandr Voronoy <servermed@gmail.com>
2017-10-22 17:24:20 +0300

    Initial Commit

---
+++ MultiWindowsApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiWindowsApp
+//
+//  Created by Sasha Voronoy on 22.10.2017.
+//  Copyright © 2017 Sasha Voronoy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
